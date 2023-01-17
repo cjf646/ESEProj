7bbe07af924ede4d5f276ed41f2416ef6669727e
cjf646 <43555542+cjf646@users.noreply.github.com>
2023-01-16 23:17:13 -0600

    updated

--- ESE_Final_Project/lcdScreen.py
+++ ESE_Final_Project/lcdScreen.py
@@ -4,6 +4,7 @@ from RPLCD import i2c
 # Import sleep library
 from time import sleep
 from activitiesAlarmSetup import *
+from doActivities import *
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
@@ -13,7 +14,7 @@ next_activity = 0
 # select_button = False
 activity_added = 0
 # constants to initialise the LCD
-
+activities_list = []
 
 lcdmode = 'i2c'
 cols = 20
@@ -28,7 +29,14 @@ port = 1 # 0 on an older Raspberry Pi
 # Initialise the LCD
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
-def run_screen():
+
+activity1 = 'Get 5 hours of sun'
+activity2 = 'Journal for 5 mins'
+activity3 = '10 pushups'
+activity4 = 'Drink glass of water'
+
+# Selecting activities
+def run_screen(ev=None):
     global next_activity
     next_activity = 0
     lcd.close(clear=True)
@@ -37,11 +45,11 @@ def run_screen():
     lcd.write_string('')
     lcd.crlf()
 
-    lcd.write_string('Get 5 hours of sun')
-#     switch2()
+    lcd.write_string(activity1)
+
     detectNextActivityPress()
     detectActivitySelect()
-    
+
 #     detectActivitySelect()
 #     lcd.backlight_enabled = False
 #     # Clear the LCD screen
@@ -54,15 +62,11 @@ def changeActivity():
         lcd.crlf()
         lcd.write_string('')
         lcd.crlf()
-        lcd.write_string('Journal for 3 hours')
-        
+        lcd.write_string(activity2)
+
         detectNextActivityPress()
         detectActivitySelect()
-        
-#         detectActivitySelect()
-        
-        
-#         sleep(5)
+
 #     # Switch off backlight
 #         lcd.backlight_enabled = False
 #     # Clear the LCD screen
@@ -73,25 +77,23 @@ def changeActivity():
         lcd.crlf()
         lcd.write_string('')
         lcd.crlf()
-        lcd.write_string('1000 pushups')
-#         GPIO.remove_event_detect(13)
+        lcd.write_string(activity3)
+
         detectNextActivityPress()
         detectActivitySelect()
-        
-#         detectActivitySelect()
-        
+
     if next_activity == 3:
         lcd.close(clear=True)
         lcd.write_string('Next one!')
         lcd.crlf()
         lcd.write_string('')
         lcd.crlf()
-        lcd.write_string('Take a shit')
-#         GPIO.remove_event_detect(13)
+        lcd.write_string(activity4)
+
         detectNextActivityPress()
         detectActivitySelect()
-        
-        
+
+# if no activities are selected then return to selecting activites again.
     if next_activity == 4 and activity_added == 0:
         lcd.close(clear=True)
         lcd.write_string('Select ATLEAST one!')
@@ -99,47 +101,165 @@ def changeActivity():
         lcd.write_string('')
         lcd.crlf()
         lcd.write_string('')
-        sleep(4)
+        sleep(3)
         run_screen()
-        
-#         GPIO.remove_event_detect(13)
-#         detectActivitySelect()
-        
-        
-    # Switch off backlight
-#         lcd.backlight_enabled = False
-#     # Clear the LCD screen
-#         lcd.close(clear=True)
+
+
+# Display the activities list selected if all activities are viewed or if the activities added are equal to 3
+    if next_activity >=4 or activity_added == 3:
+        completeActivities(activities_list, activity_added, next_activity)
+
 
 def addActivity():
+    global activities_list
     if activity_added == 1 and next_activity == 0:
-        store = 'Get 5 minutes of sun'
+        lcd.close(clear=True)
+        store_activity = activity1
+        activities_list.append(store_activity)
+        lcd.write_string(activity1)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
         lcd.write_string('')
         lcd.crlf()
-        lcd.write_string('SELECTED')
+        lcd.write_string('Press red button')
+#         detectNextActivityPress()
+#         detectActivitySelect()
+
+    if activity_added == 1 and next_activity == 1:
+        lcd.close(clear=True)
+        store_activity = activity2
+        activities_list.append(store_activity)
+        lcd.write_string(activity2)
+        lcd.crlf()
+        lcd.write_string('ADDED')
         lcd.crlf()
         lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+#         detectNextActivityPress()
+#         detectActivitySelect()
+
+    if activity_added == 1 and next_activity == 2:
+        lcd.close(clear=True)
+        store_activity = activity3
+        activities_list.append(store_activity)
+        lcd.write_string(activity3)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
+        lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+
+    if activity_added == 1 and next_activity == 3:
+        lcd.close(clear=True)
+        store_activity = activity4
+        activities_list.append(store_activity)
+        lcd.write_string(activity4)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
+        lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+
     if activity_added == 2 and next_activity == 1:
-        store = 'Journal for 5 minutes'
+        lcd.close(clear=True)
+        store_activity = activity2
+        activities_list.append(store_activity)
+        lcd.write_string(activity2)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
         lcd.write_string('')
         lcd.crlf()
-        lcd.write_string('SELECTED')
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
+    if activity_added == 2 and next_activity == 2:
+        lcd.close(clear=True)
+        store_activity = activity3
+        activities_list.append(store_activity)
+        lcd.write_string(activity3)
+        lcd.crlf()
+        lcd.write_string('ADDED')
         lcd.crlf()
         lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
+    if activity_added == 2 and next_activity == 3:
+        lcd.close(clear=True)
+        store_activity = activity4
+        activities_list.append(store_activity)
+        lcd.write_string(activity4)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
+        lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
     if activity_added == 3 and next_activity == 2:
-        store = '10 pushups'
-        lcd.write_string('')
+        lcd.close(clear=True)
+        store_activity = activity3
+        activities_list.append(store_activity)
+        lcd.write_string(activity3)
         lcd.crlf()
-        lcd.write_string('SELECTED')
-        lcd.crlf()
-        lcd.write_string('')
-    if activity_added == 3 and next_activity == 2:
-        store = 'Drink glass of water'
-        lcd.write_string('')
-        lcd.crlf()
-        lcd.write_string('SELECTED')
+        lcd.write_string('ADDED')
         lcd.crlf()
         lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
+    if activity_added == 3 and next_activity == 3:
+        lcd.close(clear=True)
+        store_activity = activity4
+        activities_list.append(store_activity)
+        lcd.write_string(activity4)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
+        lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
+
+    if activity_added == 4 and next_activity == 3:
+        lcd.close(clear=True)
+        store_activity = activity4
+        activities_list.append(store_activity)
+        lcd.write_string(activity4)
+        lcd.crlf()
+        lcd.write_string('ADDED')
+        lcd.crlf()
+        lcd.write_string('')
+        lcd.crlf()
+        lcd.write_string('Press red button ')
+        detectNextActivityPress()
+        detectActivitySelect()
+        print("hello", activities_list)
+
+
 def switch2(ev=None):
     next_button = False
 #     global next_button, count2
@@ -147,10 +267,10 @@ def switch2(ev=None):
     next_button = not next_button
     next_activity += 1
     print(next_activity)
-    
+
     if next_button == True:
         changeActivity()
-        sleep(2)
+        sleep(1)
 #             lcd.write_string('Next one!')
 #             lcd.crlf()
 #             lcd.write_string('')
@@ -164,26 +284,28 @@ def switch2(ev=None):
 
 
 def switch3(ev=None):
+
     select_button = False
 #     global select_button, activity
     global activity_added
     select_button = not select_button
-    activity_added += 1
-    print("activity added!!")
-    
+
+
     if select_button == True:
+        activity_added += 1
+        print("activities added =", activity_added)
         addActivity()
-        sleep(2)
-        
-        
+        sleep(1)
+
+
 def detectNextActivityPress():
     GPIO.remove_event_detect(13)
     GPIO.add_event_detect(13, GPIO.FALLING, callback=switch2, bouncetime=300)
-    
+
 
 def detectActivitySelect():
     GPIO.remove_event_detect(6)
-    GPIO.add_event_detect(6, GPIO.FALLING, callback=switch3, bouncetime=300)
+    GPIO.add_event_detect(6, GPIO.FALLING, callback=switch3, bouncetime=1500)
 #     lcd.write_string('Phppot')
 #     sleep(5)
     # Switch off backlight

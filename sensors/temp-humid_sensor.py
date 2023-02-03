955737edb9d51bf302a81202d046da0a83ba8afe
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-02-03 10:39:58 -0600

    Update temp-humid_sensor.py

--- sensors/temp-humid_sensor.py
+++ sensors/temp-humid_sensor.py
@@ -2,14 +2,14 @@ import Adafruit_DHT
 import time
 from datetime import datetime
 
-DHT_SENSOR = Adafruit DHT.DHT11
+DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 counter = 0
 sum_temp = 0.0
 sum_humid = 0.0
 
 while True:
-  humidity, temperature = Adafruit_DHT.read(DHT SENSOR, DHT PIN)
+  humidity, temperature = Adafruit_DHT.read(DHT SENSOR, DHT_PIN)
   if humidity is not None and temperature is not None:
     print("Temperature={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
     temp = str(temperature) 

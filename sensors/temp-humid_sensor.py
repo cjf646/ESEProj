317b8f987d93ba10dc841adfbd8cb020bbd8bd5c
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-02-03 10:36:48 -0600

    Create temp-humid_sensor.py

---
+++ sensors/temp-humid_sensor.py
@@ -0,0 +1,32 @@
+import Adafruit_DHT 
+import time
+from datetime import datetime
+
+DHT_SENSOR = Adafruit DHT.DHT11
+DHT_PIN = 4
+counter = 0
+sum_temp = 0.0
+sum_humid = 0.0
+
+while True:
+  humidity, temperature = Adafruit_DHT.read(DHT SENSOR, DHT PIN)
+  if humidity is not None and temperature is not None:
+    print("Temperature={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+    temp = str(temperature) 
+    humid = str(humidity)
+    conv_temp = float(temp) 
+    conv_humid = float(humid) 
+    file = open("temp-humid_log.txt", "a") 
+    file.write("Temperature={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity) + "\n")
+    sum_temp = sum_temp + conv_temp 
+    sum_humid = sum_humid + conv_humid
+    counter += 1
+    average_temp = sum_temp/counter 
+    average_humid = sum_humid/counter
+    file.write("Average Temperature={0:0.1f}C Average Humidity={1:0.1f}%".format(average_temp, average_humid) + "\n") 
+    print("Average Temperature={0:0.1f}C Average Humidity={1:0.1f}%".format(average temp, average_humid))
+    now = (datetime.now())
+    tstamp = "{0:%Y}-{0:%m}-{0:%d}_{0:%H}.{0:%M}.{0:%S}".format(now)
+    file.write(tstamp + "\n" + "\n")
+  time.sleep(1);
+file.close()

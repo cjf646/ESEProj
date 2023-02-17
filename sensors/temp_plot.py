3a62eda98dc8037bf4e81aacefe538a1447c0d0a
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-02-17 10:52:11 -0600

    Update temp_plot.py

--- sensors/temp_plot.py
+++ sensors/temp_plot.py
@@ -10,7 +10,7 @@ tstamp = "{0:%Y}_{0:%m}_{0:%d}".format(now)
 x = data[:, 2]
 y = data[:, 0]
 plt.title('Temperature Graph for ' + tstamp)
-plt.ylabl('Temp (deg C)')
+plt.ylabl('Temperature (deg C)')
 plt.xlabel('Time')
 plt.plot(x, y, 'r--')
 plt.show()

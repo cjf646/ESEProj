d484f0bec81b81a47dcdffc9590ae92fc19b8187
cjf646 <43555542+cjf646@users.noreply.github.com>
2023-02-24 04:38:54 -0600

    updates

--- ESE_Final_Project/recognizeFeelings.py
+++ ESE_Final_Project/recognizeFeelings.py
@@ -3,7 +3,12 @@ from activitiesAlarmSetup import *
 from lcdScreen import *
 from doActivities import *
 from sleep import *
-from activitiesAlarmSetup import *
+from morning import *
+from recognizeFeelings import *
+from parentVerification import *
+from boxStart import *
+from streak import *
+from main import *
 
 # Import LCD library
 from RPLCD import i2c
@@ -19,9 +24,10 @@ from time import sleep
 import os
 
 import pyrebase
-
+from firebase import firebase
 # select_button = False
 activity_added = 0
+activity_done = 0
 # constants to initialise the LCD
 activities_list = []
 
@@ -39,21 +45,88 @@ port = 1 # 0 on an older Raspberry Pi
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
 
+import RPi.GPIO as GPIO
+# GPIO.setwarnings(False)
 
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-def feelingRecognizer():
-
-    engine = pyttsx3.init()
-    voice = engine.getProperty('voices')
-    engine.setProperty('voice', voice[32].id)
-
-
-
-
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[32].id)
+def mood():
+    
+    
     engine.say("Select one of the mood pushbuttons that indicate how you are feeling right now")
     engine.runAndWait()
+    feelingRecognizer()
+ 
+def button_pressed(channel):
+    if channel == 17:
+        print("SAD pressed")
+    elif channel == 27:
+        print("READY pressed")
+    global keep_running
+    keep_running = False
 
+# def feelingRecognizer():
+#     global keep_running
+#     keep_running = True
+#     print("hello")
+# #     GPIO.setmode(GPIO.BCM)
+#     GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# 
+#     sadPress()
+#     readyPress()
+#     
+#     while keep_running:
+#         
+#         time.sleep(1)
+#         feelingRecognizer()
+#     # Clean up GPIO
+#     GPIO.cleanup()
+def feelingRecognizer():
+    global keep_running
+    keep_running = True
+    print("hello")
+    # GPIO.setmode(GPIO.BCM)
+    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    sad_pressed = False
+    ready_pressed = False
+    happy_pressed = False
+    excited_pressed = False
+    worried_pressed = False
+    angry_pressed = False
+    _pressed = False
+
+    while not (sad_pressed and ready_pressed):
+        if GPIO.input(17) == GPIO.LOW:
+            print("SAD pressed")
+            feeling = "Sad"
+            camera(feeling)
+            sad_pressed = True
+        if GPIO.input(27) == GPIO.LOW:
+            print("READY pressed")
+            feeling = "Ready"
+            camera(feeling)
+            ready_pressed = True
+        time.sleep(0.1)
+
+    # Clean up GPIO
+    GPIO.cleanup()
+
+def camera(feeling):    
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    engine.say("Now draw how you are feeling or what is on your mind on body map, then press green button again.")
+    engine.runAndWait()
+    
     firebaseConfig = {
   'apiKey': "AIzaSyBPmuCMq_v2euR4n4qW1hBnosQuBTgtW5k",
   'authDomain': "habits-b5b42.firebaseapp.com",
@@ -70,6 +143,8 @@ def feelingRecognizer():
     storage = firebase.storage()
 
     camera = PiCamera()
+    
+    db = firebase.database()
 
     while True:
       try:
@@ -81,13 +156,37 @@ def feelingRecognizer():
             camera.capture(name)
             print(name+" saved")
             storage.child(name).put(name)
+            storage.child(name).put(name)
             url = storage.child(name).get_url(None)
-            print("Image sent. URL:", url)
             print("Image sent")
+            print(url)
             os.remove(name)
             print("File Removed")
+            return_data = db.child('Users').get()
+            all_data = return_data.val()
+            print("Hello")      
+            days = []
+            print("Hello")
+            for x, y in all_data.items():
+                days.append(x)
+            print("Hello")
+            print(feeling)
+            work = {'Drawing Link': url, 'Feeling': feeling}
+            db.child("Users").child(days[-2]).update(work)
+            print("Hello")
             sleep(5)
-
+            break
 
       except:
-            camera.close()
+            camera.close()   
+        
+    parentFob()
+    
+    
+# def readyPress():
+#     GPIO.remove_event_detect(27)
+#     GPIO.add_event_detect(27, GPIO.FALLING, callback=button_pressed, bouncetime=300)
+# def sadPress():
+#     GPIO.remove_event_detect(17)
+#     GPIO.add_event_detect(17, GPIO.FALLING, callback=button_pressed, bouncetime=300)
+#     

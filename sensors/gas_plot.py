48319255ec59c264aa98b17b431d204987c8bd6d
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 17:59:34 -0600

    Create gas_plot.py

---
+++ sensors/gas_plot.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from datetime import datetime
+
+data = np.loadtxt('gas_log.txt')
+now = (datetime.now())
+tstamp = "{0:%Y},{0:%m},{0:%d}".format(now)
+
+x = data[:, 3]
+y1 = data[:, 0]
+y2 = data[:, 1]
+y3 = data[:, 2]
+plt.title('Gas Graph for ' + tstamp)
+plt.ylabl('Concentration (ppm)')
+plt.xlabel('Time')
+plt.plot(x, y1, 'r--')
+plt.plot(x, y2, 'r--')
+plt.plot(x, y3, 'r--')
+plt.show()
+plt.savefig(tstamp + '_gas.png'

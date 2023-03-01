c4ab63579a408113121133c0ca0be8a5de31f56c
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 17:36:14 -0600

    Updated to properly send data to text file

--- sensors/light_sensor.py
+++ sensors/light_sensor.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-import RP1.GPIO as GPIO 
+import RPi.GPIO as GPIO 
 import time
 
-readPIN 14
-counter 0
-value 0
-conv 0
+readPIN = 14
+counter = 0
+value = 0
+conv = 0
 
 GPIO.setwarnings (False)
 GPIO.setmode (GPIO.BCM) 
@@ -14,18 +14,18 @@ GPIO.setwarnings(True)
 
 try:
   while True:
-    value = str(GPIO.input (readPIN))
+    value = str(GPIO.input(readPIN))
     conv = int(value)
     if(conv == 1): 
       counter += 1
       print(counter)
-    now = (datetime.now())
-    tstamp = "{0:%Y}-{0:%m}-{0:%d}_{0:%H}.{0:%M}.{0:%S}".format(now)
-    filename = tstamp + "light_log.txt"
+      log_value = '1'
+    else:
+      log_value = '0'
+    now = datetime.now()
+    tstamp = "{0:%H}{0:%M}{0:%S}".format(now)
     file = open("light_log.txt", "a")
-    file.write("Light detected " + tstamp + "\n")
+    file.write(log_value + " " + tstamp + "\n")
     time.sleep(1) 
-  file.close()
-except KeyboardInterrupt:
-  print('interuppted')
+finally:
   GPIO.cleanup()

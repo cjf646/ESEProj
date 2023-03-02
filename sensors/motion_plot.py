3fb69a06853e501777cfb0643933afaa61a89ad5
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 19:20:30 -0600

    Updated to better display time values on x-axis

--- sensors/motion_plot.py
+++ sensors/motion_plot.py
@@ -7,11 +7,16 @@ data = np.loadtxt('motion_log.txt')
 now = (datetime.now())
 tstamp = "{0:%Y},{0:%m},{0:%d}".format(now)
 
-x = data[:, 1]
+x = []
+for timestamp in data[:, 1]:
+    time_str = str(int(timestamp))
+    formatted_time_str = ':'.join([time_str[i:i+2] for i in range(0, len(time_str), 2)])
+    x.append(formatted_time_str)
 y = data[:, 0]
 plt.title('Motion Graph for ' + tstamp)
 plt.ylabel('Movement')
 plt.xlabel('Time')
 plt.plot(x, y, 'r--')
+plt.xticks(rotation=45, ha='right')
 plt.savefig(tstamp + '_motion.png')
 plt.show()

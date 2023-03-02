98809802628bad37106b2a44a2c1a6d9db8302d2
cjf646 <43555542+cjf646@users.noreply.github.com>
2023-03-02 15:08:50 -0600

    Updated for Curti's Raspberry Pi

--- sensors/temp-humid_sensor.py
+++ sensors/temp-humid_sensor.py
@@ -1,31 +1,41 @@
-import Adafruit_DHT 
 import time
+import board
+import adafruit_dht
 from datetime import datetime
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+dhtDevice = adafruit_dht.DHT11(board.D4)
 counter = 0
 sum_temp = 0.0
 sum_humid = 0.0
 
 while True:
-  humidity, temperature = Adafruit_DHT.read(DHT SENSOR, DHT_PIN)
-  if humidity is not None and temperature is not None:
-    print("Temperature={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
-    temp = str(temperature) 
-    humid = str(humidity)
-    conv_temp = float(temp) 
-    conv_humid = float(humid) 
-    file = open("temp-humid_log.txt", "a") 
-    file.write(temp + " ")
-    sum_temp = sum_temp + conv_temp 
-    sum_humid = sum_humid + conv_humid
-    counter += 1
-    average_temp = sum_temp/counter 
-    average_humid = sum_humid/counter
-    print("Average Temperature={0:0.1f}C Average Humidity={1:0.1f}%".format(average temp, average_humid))
-    now = (datetime.now())
-    tstamp = "{0:%H}{0:%M}{0:%S}".format(now)
-    file.write(tstamp + "\n")
-  time.sleep(1);
+    try:
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f} C    Humidity: {}% ".format(temperature_c, humidity))
+        temp = str(temperature_c) 
+        humid = str(humidity)
+        conv_temp = float(temp) 
+        conv_humid = float(humid) 
+        file = open("temp-humid_log.txt", "a") 
+        file.write(temp + " ")
+        sum_temp = sum_temp + conv_temp 
+        sum_humid = sum_humid + conv_humid
+        counter += 1
+        average_temp = sum_temp/counter 
+        average_humid = sum_humid/counter
+        print("Average Temperature={0:0.1f}C Average Humidity={1:0.1f}%".format(average_temp, average_humid))
+        now = (datetime.now())
+        tstamp = "{0:%H}{0:%M}{0:%S}".format(now)
+        file.write(tstamp + "\n")
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
 file.close()

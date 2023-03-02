2a797ba29e5376919bc5a30b91cca68fb480def6
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 18:00:01 -0600

    typo

--- sensors/humid_plot.py
+++ sensors/humid_plot.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 data = np.loadtxt('temp-humid_log.txt')
 now = (datetime.now())
-tstamp = "{0:%Y}_{0:%m}_{0:%d}".format(now)
+tstamp = "{0:%Y},{0:%m},{0:%d}".format(now)
 
 x = data[:, 2]
 y = data[:, 1]
 plt.title('Humidity Graph for ' + tstamp)
-plt.ylabl('THumidity (%)')
+plt.ylabl('Humidity (%)')
 plt.xlabel('Time')
 plt.plot(x, y, 'r--')
 plt.show()

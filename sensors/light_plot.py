8eef863c82fd36830931da1742c08517accf2b88
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 18:07:41 -0600

    Create light_plot.py

---
+++ sensors/light_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from datetime import datetime
+
+data = np.loadtxt('light_log.txt')
+now = (datetime.now())
+tstamp = "{0:%Y},{0:%m},{0:%d}".format(now)
+
+x = data[:, 1]
+y = data[:, 0]
+plt.title('Light Graph for ' + tstamp)
+plt.ylabl('Light Threshold')
+plt.xlabel('Time')
+plt.plot(x, y, 'r--')
+plt.show()
+plt.savefig(tstamp + '_light.png')

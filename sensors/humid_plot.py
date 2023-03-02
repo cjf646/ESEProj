b782b839ff29e2d5a9b8992dd344101b1db126d7
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-03-01 19:22:38 -0600

    typo

--- sensors/humid_plot.py
+++ sensors/humid_plot.py
@@ -8,7 +8,7 @@ now = (datetime.now())
 tstamp = "{0:%Y},{0:%m},{0:%d}".format(now)
 
 x = []
-for timestamp in data[:, 1]:
+for timestamp in data[:, 2]:
     time_str = str(int(timestamp))
     formatted_time_str = ':'.join([time_str[i:i+2] for i in range(0, len(time_str), 2)])
     x.append(formatted_time_str)

fcab455e0a7b17e7c6b587907fd3b1a5c58b230e
cjf646 <43555542+cjf646@users.noreply.github.com>
2023-03-02 23:29:20 -0600

    updated

--- ESE_Final_Project/doActivities.py
+++ ESE_Final_Project/doActivities.py
@@ -3,7 +3,12 @@ from activitiesAlarmSetup import *
 from lcdScreen import *
 from doActivities import *
 from sleep import *
-
+from morning import *
+from recognizeFeelings import *
+from parentVerification import *
+from boxStart import *
+from streak import *
+from main import *
 
 
 from time import sleep
@@ -22,6 +27,24 @@ port = 1 # 0 on an older Raspberry Pi
 
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
+
+
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+from firebase import firebase
+import pyttsx3
+import time
+import pyrebase
+
+#date module
+import datetime
+
+
+
+
+
 
 
 
@@ -51,25 +74,17 @@ def completeActivities(activities_list, activity_added, next_activity):
     all_data = return_data.val()
     days = []
     alarm_times = []
+#     if all_data == None:
     for x, y in all_data.items():
         days.append(x)
-        print("what isd this", days)
         alarm_times.append(y)
-        print("hello:", alarm_times)
 
     alarm_time = alarm_times[-2]
-    print("working?", alarm_time)
     for x, y in alarm_time.items():
         if x == 'Alarm Time hour':
             hour = y
         if x == 'Alarm time minute':
             minute = y
-    print(hour)
-    print(minute)
-
-
-#     data = {'Activity 1': activities_list[0]}
-#     db.child("Users").child(days[-2]).set(data)
 
     lcd.close(clear=True)
     lcd.write_string('Activity List')
@@ -94,7 +109,67 @@ def completeActivities(activities_list, activity_added, next_activity):
         db.child("Users").child(days[-2]).set(data[2])
 
 
+    engine = pyttsx3.init()
+    voice = engine.getProperty('voices')
+    engine.setProperty('voice', voice[10].id)
+
+    engine.say("Hi, I am your superhero coach")
+    engine.runAndWait()
+    time.sleep(1)
+    engine.say("Please say one thing you are grateful for before you go to sleep! ok go")
+    engine.runAndWait()
+
+    gratitudeVoiceDeviceInteraction()
 
 
-    clockRunningNow()
-#     gratitudeVoiceDeviceInteraction()
+def listening():
+    listening = True
+
+    while listening:
+    #keyword = "activate"
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            recognizer.adjust_for_ambient_noise(source)
+            recognizer.dynamic_energy_threshold = 2500
+
+
+            try:
+                print("Listening...")
+                GPIO.output(26, GPIO.HIGH)
+                audio = recognizer.listen(source)
+                GPIO.output(26, GPIO.LOW)
+                superhero_name = recognizer.recognize_google(audio)
+
+                return superhero_name
+
+            except sr.UnknownValueError:
+                print("Didn't recognize that.")
+
+
+
+
+def gratitudeVoiceDeviceInteraction():
+    engine = pyttsx3.init()
+    voice = engine.getProperty('voices')
+    engine.setProperty('rate', 150)
+    engine.setProperty('voice', voice[10].id)
+    engine.say("Hi, I am your superhero coach")
+    engine.runAndWait()
+    gratitude_text = listening()
+    substring = "I am grateful"
+    if substring in gratitude_text:
+
+        engine.say("Did you say:" + gratitude_text)
+        engine.runAndWait()
+        yes_or_no = listening()
+        yes = "yes"
+        if yes in yes_or_no:
+            engine.say("Yes you are very grateful for that. Have a great sleep!")
+            engine.runAndWait()
+
+            clockRunningNow()
+    else:
+        engine.say("That is not what you are supposed to say. Try again")
+        engine.runAndWait()
+        gratitudeVoiceDeviceInteraction()
+    time.sleep(1)
